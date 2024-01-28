4ceea6a5d97946b249d8706aa0e8452492f71496
marcosc1995 <marcos_c95@outlook.com>
2024-01-28 16:47:13 -0300

    only a test
    change the type of element

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function printList(item) {
       printList(item);
     });
     // DELETE BUTTON OF THE LIST ITEM
-    const btnDel = document.createElement('div');
+    const btnDel = document.createElement('button');
     btnDel.textContent = 'X'
     btnDel.classList = "btn";
     btnDel.addEventListener("click", (e) => {
